808e1bbbbc63d20453668deb1149f58bfb4d8459
Erick Gomes <erickjufes@gmail.com>
2021-04-19 14:47:05 -0300

    First controller and Razor page tests.

--- SalesWebMVC/Controllers/HomeController.cs
+++ SalesWebMVC/Controllers/HomeController.cs
@@ -17,7 +17,8 @@ namespace SalesWebMVC.Controllers
 
         public IActionResult About()
         {
-            ViewData["Message"] = "Your application description page.";
+            ViewData["Message"] = "Sales Web MCV App from C# Course.";
+            ViewData["Student"] = "Aluno: Erick Gomes";
 
             return View();
         }
